393a177a52b8e9d273371a57413fe88a4f995791
StrangerWS <strangernf@gmail.com>
2016-11-02 09:05:34 +0300

    Game of Numbers bugfixes

--- src/com/netcracker/edu/ssu/dobrynin/artemiy/task002/GameApp.java
+++ src/com/netcracker/edu/ssu/dobrynin/artemiy/task002/GameApp.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
  * Created by User on 20.10.2016.
  */
 public class GameApp {
-    public static final String VERSION = "1.0.0.4";
+    public static final String VERSION = "1.0.2.0";
     public static final String DEVELOPER = "StrangerWS | Wicked Studio";
 
     public static void main(String[] args) {
